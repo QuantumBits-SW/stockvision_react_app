00fb27ae5099ef0f5d087ec1786849e3db7b9df9
veerpalk1 <veerpal17011999@gmail.com>
2025-02-20 20:04:07 -0500

    initial changes

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,8 +1,27 @@
+import InputField from "./InputField";
+
 const Register = () => {
   return ( 
-    <div>
-      Register
-    </div>
+    <div className="flex flex-col justify-center animate-slideInFromRight">
+    <form>
+        <InputField autoComplete="email" label="Email Address" Icon={MailOutlinedIcon} onChange={onChange} checked={isValid}/>
+        <InputField autoComplete="password" label="Password" Icon={PasswordOutlinedIcon} onChange={onChange}/>
+        <Button
+          variant="contained"
+          className={`!rounded-[30px] w-full !mt-2 !p-2 text-white cursor-pointer ${(isValid && data.password !== "") && "!bg-black"}`}
+          disabled={!(isValid && data.password !== "")}
+          sx={{
+            '&.MuiButton-root': {
+              '&.Mui-disabled': {
+                backgroundColor: "rgba(0, 0, 0, 0.2) !important"
+              }
+            }
+          }}
+        >
+          Continue
+        </Button>
+    </form>
+  </div>
   )
 }
 
