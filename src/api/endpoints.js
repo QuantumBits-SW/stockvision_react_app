cf598d220b7e76990e2d970f0ab6a9b3e1be3362
jaivik1920 <pateljaivik2025@gmail.com>
2025-02-21 14:38:27 -0500

    git commit -m "SCRUM-27 Login Integration with firebase and Backend"

---
+++ src/api/endpoints.js
@@ -0,0 +1,5 @@
+const API_BASE_URL = 'http://localhost:7070/stockvision';
+
+export const API_ENDPOINTS = {
+  getUserProfile: `${API_BASE_URL}/getUserProfile`,
+};
--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -3,6 +3,7 @@ import MailOutlinedIcon from '@mui/icons-material/MailOutlined';
 import PasswordOutlinedIcon from '@mui/icons-material/PasswordOutlined';
 import InputField from "./InputField";
 import { useState } from "react";
+import { loginUser, getUserProfile } from "../../services/loginService";
 
 const Login = () => {
   const [data, setData] = useState({
@@ -11,6 +12,7 @@ const Login = () => {
   })
 
   const [isValid, setIsValid] = useState(false);
+  const [error, setError] = useState(null);
 
   function isValidEmail(email) {
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -25,12 +27,26 @@ const Login = () => {
     }(): name === "Password" ? setData(data => ({ ...data, password: value })) : null
   }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const token = await loginUser(data.email, data.password);
+      const userData = await getUserProfile(token, {});
+      console.log("User Data:", userData);
+    } catch (err) {
+      console.error("Error during login process:", err);
+      setError("Login failed, Please check your credentials!");
+    }
+  };
+
   return (
   <div className="flex flex-col justify-center animate-slideInFromRight">
-    <form>
+    <form onSubmit={handleSubmit}>
         <InputField autoComplete="email" label="Email Address" Icon={MailOutlinedIcon} onChange={onChange} checked={isValid}/>
         <InputField autoComplete="password" label="Password" Icon={PasswordOutlinedIcon} onChange={onChange}/>
+        {error && <p className="text-center text-red-500">{error}</p>}
         <Button
+          type="submit"
           variant="contained"
           className={`!rounded-[30px] w-full !mt-2 !p-2 text-white cursor-pointer ${(isValid && data.password !== "") && "!bg-black"}`}
           disabled={!(isValid && data.password !== "")}
