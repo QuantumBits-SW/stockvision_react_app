db315c4935c1ecc5b9328d368ece50f2be219f5b
git-nitin01 <nitin.sudo098@gmail.com>
2025-03-16 20:06:11 -0400

    fix-private-route

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const PrivateRoute = ({ element }) => {
-  const { user } = useSelector((state) => state.auth.user);
+  const { user } = useSelector((state) => state.auth);
 
   return user ? element : <Navigate to="/" />;
 };
